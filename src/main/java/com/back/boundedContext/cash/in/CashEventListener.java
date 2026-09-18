d5c5ecf2c4be4adf0cc9b67d7fbfee868edf3841
khb <khb3025@gmail.com>
2026-09-18 16:24:24 +0900

    0023

---
+++ src/main/java/com/back/boundedContext/cash/in/CashEventListener.java
@@ -0,0 +1,30 @@
+package com.back.boundedContext.cash.in;
+
+import com.back.boundedContext.cash.app.CashFacade;
+import com.back.shared.member.event.MemberJoinedEvent;
+import com.back.shared.member.event.MemberModifiedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
+
+@Component
+@RequiredArgsConstructor
+public class CashEventListener {
+    private final CashFacade cashPacade;
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void handle(MemberJoinedEvent event){
+        cashPacade.syncMember(event.getMember());
+    }
+
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    @Transactional (propagation = Propagation.REQUIRES_NEW)
+    public void handle(MemberModifiedEvent event){
+       cashPacade.syncMember(event.getMember());
+    }
+
+}
